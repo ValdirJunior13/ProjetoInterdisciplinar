47e2f76063db4fe81fc823110683236c151c0d92
Valdirjunior12 <66043046+ValdirJunior13@users.noreply.github.com>
2023-03-18 04:10:30 +0000

    teste2

--- TesteOpenAi/testeOpenAi.py
+++ TesteOpenAi/testeOpenAi.py
@@ -1,29 +1,55 @@
-import openai  
+import openai
+import csv
+import pandas as pd
+# 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
+import openai
+import pandas as pd
 
-openai.api_key = "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"
+# Define a chave da API da OpenAI
+openai.api_key = "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"
 
-model_engine = "text-davinci-003"
+# Define o modelo da OpenAI a ser usado pelo chatbot
+model_engine = "text-davinci-002"
 
+# Abre o arquivo CSV com as informações
+df = pd.read_csv("dados_uf.csv")
+
+# Função que processa a entrada do usuário e retorna uma resposta baseada nas informações
+
+
+def processar_entrada(input_text):
+    input_text = input_text.lower()
+
+    if "ufrpe" in input_text:
+        resposta = df[df["informacao"] == "ufrpe"]["valor"].values[0]
+    elif "matriculas" in input_text:
+        resposta = "Seu número de matrícula é: {}".format(
+            df[df["informacao"] == "matricula"]["valor"].values[0])
+    elif "calendario" in input_text:
+        resposta = df[df["informacao"] == "calendario"]["valor"].values[0]
+    else:
+        resposta = "Desculpe, não entendi o que você quis dizer."
+
+    return resposta
+
+
+# Loop principal do bot
 while True:
-    print (50*"-")
-    prompt = input("Digite o texto: ")
-    print('.')
-    print('..')
-    print('...')
-    print('processando...')
-    print(50*"-")
-    completion = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        temperature=1.0,
-        max_tokens=1024
+    # Recebe uma entrada do usuário
+    user_input = input("Usuário: ")
 
+    # Processa a entrada do usuário e obtém a resposta baseada nas informações
+    bot_response = processar_entrada(user_input)
+
+    # Faz a chamada à API da OpenAI para obter uma resposta mais completa
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=bot_response,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7,
     )
-    print (50*"-")
-    response = completion.choices[0].text
-    print(response)
-    print (50*"-")
-    print (50*"-")
-    saida = input("Deseja continuar? (S/N): ")
-    if saida == "N":
-        break
+
+    # Exibe a resposta completa do chatbot
+    print("Bot: " + response.choices[0].text.strip())
